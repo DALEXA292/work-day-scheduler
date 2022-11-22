b6689bc63de538cd33dc47b26adcc52b63fcfef1
DALEXA292 <DALEXA292@GMAIL.COM>
2022-11-22 01:03:54 -0800

    Added readme and js local storage

--- Develop/script.js
+++ Develop/script.js
@@ -29,3 +29,29 @@ $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
         })
     }
     
+    $(document).ready(function () {
+        // Btn lister
+        $(".saveBtn").on("click", function () {
+            
+            var text = $(this).siblings(".description").val();
+            var time = $(this).parent().attr("id");
+    
+            // Save in local storage
+            localStorage.setItem(time, text);
+        })
+
+
+    // obtaining items from local storage if applicable
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour1 .description").val(localStorage.getItem("hour13"));
+    $("#hour2 .description").val(localStorage.getItem("hour14"));
+    $("#hour3 .description").val(localStorage.getItem("hour15"));
+    $("#hour4 .description").val(localStorage.getItem("hour16"));
+    $("#hour5 .description").val(localStorage.getItem("hour17"));
+
+    currentTime();
+})
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Work Day Scheduler Starter Code
+ 
+# About
+The app allows users to input a work task from the hours of 9am to 5pm and save the task.  Users will be able to see the time slots in red if that time has already passed and in green if its a future time.
+ 
+# Getting started
+Users will begin by adding a task on the appropirate time slot.  Then if they wish to save the task they will press on the save button.  This will maintain the task even if the page is refreshed.
+ 
+ 
+# Technology
+HTML
+CSS
+JavaScript
+moments.js
+JQuery
+ 
+# Website
+ 
+ 
+# Contribution
+Alexa De La Cruz
